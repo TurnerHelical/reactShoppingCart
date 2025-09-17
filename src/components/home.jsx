e3ee534814539ad1f542d0afcb148933ef80221b
TurnerHelical <hunterde91@gmail.com>
2025-09-17 16:08:18 -0400

    started basic structure for the homepage

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,7 +3,16 @@
 const Home = () => {
     return (
         <>
-        <p>Homepage</p></>
+        <div>
+            <p>Text here</p>
+            <img src="#" id="homeImage"/>
+            <div id="reviewCtr">
+                <div>Testimonial</div>
+                <div>Testimonial</div>
+                <div>Testimonial</div>
+            </div>
+        </div>
+        </>
     )
 }
 
