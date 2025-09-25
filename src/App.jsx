ebc2e2ed9d3febde495503b283b8c05cbd37c8b0
TurnerHelical <hunterde91@gmail.com>
2025-09-25 16:22:42 -0400

    built out the home page and styled it

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
   return (
     <>
       <Header />
-      <div>
+      <main>
         <Outlet />
-      </div>
+      </main>
       
     </>
   )
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,19 +1,65 @@
-
+import "../styles/home.css";
 
 const Home = () => {
-    return (
-        <>
-        <div>
-            <p>Text here</p>
-            <img src="#" id="homeImage"/>
-            <div id="reviewCtr">
-                <div>Testimonial</div>
-                <div>Testimonial</div>
-                <div>Testimonial</div>
-            </div>
+  return (
+    <>
+      <div id="content">
+        <div id="welcome">
+          <img
+            src="src/assets/groceryStorefront.png"
+            id="homeImage"
+          />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+            itaque explicabo, nam incidunt vero sit. Pariatur quisquam quo
+            veniam autem ut sapiente aliquam voluptate? Quidem ratione
+            exercitationem distinctio eveniet nulla!
+          </p>
         </div>
-        </>
-    )
-}
+        <div id="reviewCtr">
+          <div id="reviewTitle">
+            <h2>"This is what our customers have to say"</h2>
+          </div>
+          <div id="reviewCardCtr">
+            <div className="reviewCard">
+              <div>
+                <h4>-Reviewer's Name</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Incidunt tempore ipsum aut provident. Iure quo officia
+                  deserunt reprehenderit? Iusto maxime, perspiciatis nisi est
+                  harum mollitia debitis perferendis praesentium nihil impedit.
+                </p>
+              </div>
+            </div>
+            <div className="reviewCard">
+              <div>
+                <h4>-Reviewer's Name</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Incidunt tempore ipsum aut provident. Iure quo officia
+                  deserunt reprehenderit? Iusto maxime, perspiciatis nisi est
+                  harum mollitia debitis perferendis praesentium nihil impedit.
+                </p>
+              </div>
+            </div>
+            <div className="reviewCard">
+              <div>
+                <h4>-Reviewer's Name</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Incidunt tempore ipsum aut provident. Iure quo officia
+                  deserunt reprehenderit? Iusto maxime, perspiciatis nisi est
+                  harum mollitia debitis perferendis praesentium nihil impedit.
+                </p>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
