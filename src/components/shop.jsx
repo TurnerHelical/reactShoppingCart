e086c15e3ec9f731897c01568494e809edb03e09
TurnerHelical <hunterde91@gmail.com>
2025-09-30 10:47:22 -0400

    created base layout and product card for the shop page

--- src/components/shop.jsx
+++ src/components/shop.jsx
@@ -1,7 +1,25 @@
+import '../styles/shop.css'
+
 const Shop = () => {
     return (
         <>
-        <p>shop page</p></>
+            <div id='shopContentCtr'>
+                
+                <h2 id='shopTitle'>Check out our stuff!</h2>
+                <div id="productCardCtr">
+                    <div className="productCard">
+                        <img className='productImg' src="#" />
+                        <div className="productInfo">
+                            <h3 className="productName">Product Name Here</h3>
+                            <p className="productPrice">Price here</p>
+                            <p className="productQuant">How many in cart</p>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
