b3ec5dce88c009d88ca631c9dedd9d045e0ee312
TurnerHelical <hunterde91@gmail.com>
2025-10-01 17:44:08 -0400

    finished the cart aside logic, still need to style it more

--- src/components/shop.jsx
+++ src/components/shop.jsx
@@ -1,65 +1,78 @@
-import '../styles/shop.css'
-import { useOutletContext } from 'react-router'
-import { useState } from 'react';
-
+import "../styles/shop.css";
+import { useOutletContext } from "react-router";
+import { useState } from "react";
 
 function Shop() {
-    const {itemArray, quantityClick, inCart, cartSubtotal} = useOutletContext();
-    const [cartOpen, setCartOpen] = useState(false);
+  const { itemArray, quantityClick, inCart, cartSubtotal } = useOutletContext();
+  const [cartOpen, setCartOpen] = useState(false);
 
-
-    return (
-        <>
-            <div id='shopContentCtr'>
-               
-                
-                <h2 id='shopTitle'>Check out our stuff!</h2>
-                <div id="productCardCtr" >
-                    {itemArray.map((i,index) => (
-                    <div className="productCard" key={i.name}>
-                        <img className='productImg' src={i.image} alt={i.name}/>
-                        <div className="productInfo">
-                            <h3 className="productName" data-full={i.name}><span className="productName__text">{i.name}</span></h3>
-                            <p className="productPrice">${Number(i.price).toFixed(2)}</p>
-                            <p className="productQuant">{i.quantity} in cart</p>
-                            <div className='btnCtr'>
-                                <button className='cardBtn' onClick={() => quantityClick(index, +1)}>+</button>
-                                <button className='cardBtn' onClick={() => quantityClick(index, -1)}>-</button>
-                            </div>
-                        </div>
-
-                    </div>
-                    ))}
+  return (
+    <>
+      <div id="shopContentCtr">
+        <h2 id="shopTitle">Check out our stuff!</h2>
+        <div id="productCardCtr">
+          {itemArray.map((i, index) => (
+            <div className="productCard" key={i.name}>
+              <img className="productImg" src={i.image} alt={i.name} />
+              <div className="productInfo">
+                <h3 className="productName" data-full={i.name}>
+                  <span className="productName__text">{i.name}</span>
+                </h3>
+                <p className="productPrice">${Number(i.price).toFixed(2)}</p>
+                <p className="productQuant">{i.quantity} in cart</p>
+                <div className="btnCtr">
+                  <button
+                    className="cardBtn"
+                    onClick={() => quantityClick(index, +1)}
+                  >
+                    +
+                  </button>
+                  <button
+                    className="cardBtn"
+                    onClick={() => quantityClick(index, -1)}
+                  >
+                    -
+                  </button>
                 </div>
-                <div id='cartCtr'>
-                    <img id='cartIcon' src='https://www.freeiconspng.com/uploads/basket-cart-icon-27.png' onClick={() => setCartOpen(!cartOpen)}/>
-                    <div id='cartAside' className={`${cartOpen ? 'open' : ''}`}>
-                        <h3>Cart</h3>
-                        <div id='cartContents'>
-                            {inCart.map((i) => {
-
-                                    return (
-                                        <div>
-                                            {i.name}
-                                            <br></br>
-                                            {i.quantity}
-                                            <br></br>
-                                            ${(i.quantity * i.price).toFixed(2)}
-                                        </div>
-                                    )
-                                })
-                            }
-                            
-                            ${cartSubtotal}
-                            
-
-                        </div>
-                    </div>
-                </div>
+              </div>
             </div>
-        </>
-    )
+          ))}
+        </div>
+        <div id="cartCtr">
+          <img
+            id="cartIcon"
+            src="https://www.freeiconspng.com/uploads/basket-cart-icon-27.png"
+            onClick={() => setCartOpen(!cartOpen)}
+          />
+          <div id="cartAside" className={`${cartOpen ? "open" : ""}`}>
+            <h3>Cart</h3>
+            <div id="cartContents">
+              {inCart.length === 0 ? (
+                <p>Cart is Empty</p>
+              ) : (
+                <>
+                  {inCart.map((i) => (
+                    <div className="cartLine" key={i.name}>
+                      <div className="cartLineName">{i.name}</div>
+                      <div className="cartLineQty">Qty: {i.quantity}</div>
+                      <div className="cartLineTotal">
+                        ${(i.quantity * i.price).toFixed(2)}
+                      </div>
+                    </div>
+                  ))}
+
+                  <div className="cartFooter">
+                    <div className="cartSubtotalLabel">Subtotal</div>
+                    <div className="cartSubtotalValue">${(cartSubtotal).toFixed(2)}</div>
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Shop
-
+export default Shop;
